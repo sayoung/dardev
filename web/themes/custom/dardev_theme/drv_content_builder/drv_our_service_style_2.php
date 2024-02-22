42f668b281f45ed51acf3d74016e91d1f45529c9
sayoung <mourad.dardari@gmail.com>
2024-02-22 18:06:15 +0100

    service 2 update

--- web/themes/custom/dardev_theme/drv_content_builder/drv_our_service_style_2.php
+++ web/themes/custom/dardev_theme/drv_content_builder/drv_our_service_style_2.php
@@ -127,93 +127,71 @@ if (!class_exists("element_drv_our_service_style_2")):
             }
             ob_start();
             ?>
-                        <div class="row">    
-                            <div class="col-lg-12 mb-md-60 mb-sm-30">
-                                
-                                <h2 class="section-caption mb-xs-10"><?php print $sub_title; ?></h2>
-                                <h3 class="section-title mb-30"><span class="wow charsAnimIn" data-splitting="chars"><?php print $title; ?></span></h3>
-                                
-                                <div class="row">
-                                    <div class="col-lg-10">
-                                        <p class="section-descr mb-50 mb-sm-30 wow fadeInUp" data-wow-delay="0.4s">
-                                        <?php print $description; ?> 
-                                        </p>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="">
                             <section class="page-section pt-0 px-2 px-lg-0" id="services">                    
-                    <div class="container position-relative pt-30 bg-white round mt-n120 mt-sm-n60">
+                                <div class="container position-relative pt-30 bg-white round mt-n120 mt-sm-n60">
                         
-                        <!-- Nav tabs -->
-                        <ul class="nav nav-tabs tpl-alt-tabs mb-20 mb-xs-0 wow fadeInUp" data-wow-offset="0" role="tablist">
-								<?php for($i=1; $i<=10; $i++){ ?>
-                                   <?php 
-                                      $title = "title_{$i}";
-                                      $content = "content_{$i}";
-                                      $image = "image_{$i}";
-                                   ?>
-								   <?php if($$title){ ?>
-                            <li class="nav-item" role="presentation">
-                                <a href="#service-<?php print $i ?>" class="nav-link <?php if($i==1){ ?>active<?php }  ?>" data-bs-toggle="tab" role="tab" aria-selected="true">
-                                    
-                                    <div class="alt-tabs-icon">
-                                        <?php print $$image ?>
-                                    </div>
-                                    
-                                    <?php print $$title ?>
-                                    
-                                </a>
-                            </li>
-							<?php }  ?>   
-                                    <?php } ?>
-                        </ul>
-                        <!-- End Nav tabs -->
+                                    <!-- Nav tabs -->
+                                    <ul class="nav nav-tabs tpl-alt-tabs mb-20 mb-xs-0 wow fadeInUp" data-wow-offset="0" role="tablist">
+                                        <?php for($i=1; $i<=10; $i++){ ?>
+                                            <?php 
+                                                $title = "title_{$i}";
+                                                $content = "content_{$i}";
+                                                $image = "icon_{$i}";
+                                            ?>
+                                            <?php if($$title){ ?>
+                                                <li class="nav-item" role="presentation">
+                                                    <a href="#service-<?php print $i ?>" class="nav-link <?php if($i==1){ ?>active<?php }  ?>" data-bs-toggle="tab" role="tab" aria-selected="true">
+                                        
+                                                        <div class="alt-tabs-icon">
+                                                            <?php print $$image ?>
+                                                        </div>
+                                        
+                                                        <?php print $$title ?>
+                                        
+                                                    </a>
+                                                </li>
+                                            <?php }  ?>   
+                                        <?php } ?>
+                                    </ul>
                         
-                        <!-- Tab panes -->
-                        <div class="tab-content tpl-tabs-cont">
-                            
-                            <?php for($i=1; $i<=10; $i++){ ?>
-                                        <?php 
-                                            $title = "title_{$i}";
-                                            $content = "content_{$i}";
-                                            $image = "icon_{$i}";
-                                            $mini_description = "mini_descr_{$i}";
-                                        ?>
-                                        <?php if($$title){ ?>	
-                            <div class="tab-pane fade show active" id="service-<?php print $i ?>" role="tabpanel">
+                                    <!-- Tab panes -->
+                                    <div class="tab-content tpl-tabs-cont">
                                 
-                                <div class="row">
-                                    <div class="col-lg-4 mb-md-40 mb-xs-30 wow linesAnimIn" data-splitting="lines">
-                                        <blockquote class="mt-0 mb-0">
-                                            <p class="mb-20 mb-sm-10">
-                                                <?php print $$mini_description ?>
-                                            </p>
-                                            <footer>
-                                                Thomas Anderson
-                                            </footer>
-                                        </blockquote>
+                                            <?php for($i=1; $i<=10; $i++){ ?>
+                                                <?php 
+                                                    $title = "title_{$i}";
+                                                    $content = "content_{$i}";
+                                                    $image = "icon_{$i}";
+                                                    $mini_description = "mini_descr_{$i}";
+                                                ?>
+                                                <?php if($$title){ ?>	
+
+                                                    <div class="tab-pane fade show <?php if($i==1){ ?>active<?php }  ?>" id="service-<?php print $i ?>" role="tabpanel">
+                                                        
+                                                        <div class="row">
+                                                            <div class="col-lg-4 mb-md-40 mb-xs-30 wow linesAnimIn" data-splitting="lines">
+                                                                <blockquote class="mt-0 mb-0">
+                                                                    <p class="mb-20 mb-sm-10">
+                                                                        <?php print $$mini_description ?>
+                                                                    </p>
+                                                                    <footer>
+                                                                        Thomas Anderson
+                                                                    </footer>
+                                                                </blockquote>
+                                                            </div>
+                                                            <div class="col-md-6 col-lg-8 mb-sm-30 wow linesAnimIn" data-splitting="lines">
+                                                                <?php print $$content ?>
+                                                            </div>
+                                                        </div>                                
+                                                        
+                                                    </div>
+                                                <?php }  ?> 
+                                            <?php } ?> 
                                     </div>
-                                    <div class="col-md-6 col-lg-4 mb-sm-30 wow linesAnimIn" data-splitting="lines">
-                                        <?php print $$content ?>
-                                    </div>
-                                </div>                                
-                                
-                            </div>
-							 <?php }  ?> 
-                                    <?php } ?> 
-                            <!-- End Service Item -->
-                            
-                           
-                          
-                            
-                        </div>
-                        <!-- End Tab panes -->
                     
-                    </div>
-                </section>
-                            </div>
-                        </div>
+                                </div>
+                            </section>
+                       
                                 
                                 
                             
